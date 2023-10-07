17b03487025c3804a14c02ab891bba9b93ecf00b
TUDE RAMA <tuderama2003@gmail.com>
2023-10-07 09:20:37 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,2 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["index.html", "about.html", "project.html"],
-  darkMode: 'class',
-  theme: {
-    container:{
-      center: true,
-      padding: '16px'
-    },
-    extend: {
-      colors:{
-        primary: '#3b82f6',
-        dark: '#0f172a'
-      }
-    },
-  },
-  plugins: [],
-}
+
 
